0b60378e6b947ccec046c3cba78240809cb9657b
ruta36 <ruthdamtew2086@gmail.com>
2026-06-17 03:51:52 +0300

    Initial commit

---
+++ MiniCloud/src/main/java/com/hilcoe/MiniCloud/MiniCloudApplication.java
@@ -0,0 +1,13 @@
+package com.hilcoe.MiniCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCloudApplication.class, args);
+	}
+
+}
---
+++ MiniCloud/src/test/java/com/hilcoe/MiniCloud/MiniCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilcoe.MiniCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
